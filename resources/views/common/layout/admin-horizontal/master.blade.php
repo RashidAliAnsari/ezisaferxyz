fcd4b473f852844c807b4b52c9b0bde5148db907
rashidali33500@gmail.com <rashidali33500@gmail.com>
2021-12-08 20:55:01 +0500

    dashtic dashoards / login register pages design

--- resources/views/common/layout/admin-horizontal/master.blade.php
+++ resources/views/common/layout/admin-horizontal/master.blade.php
@@ -4,9 +4,10 @@
 		<!-- Meta data -->
 		<meta charset="UTF-8">
 		<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
-		<meta content="Dashtic - Bootstrap Webapp Responsive Dashboard Simple Admin Panel Premium HTML5 Template" name="description">
+		{{-- <meta content="Dashtic - Bootstrap Webapp Responsive Dashboard Simple Admin Panel Premium HTML5 Template" name="description">
 		<meta content="Spruko Technologies Private Limited" name="author">
-		<meta name="keywords" content="Admin, Admin Template, Dashboard, Responsive, Admin Dashboard, Bootstrap, Bootstrap 4, Clean, Backend, Jquery, Modern, Web App, Admin Panel, Ui, Premium Admin Templates, Flat, Admin Theme, Ui Kit, Bootstrap Admin, Responsive Admin, Application, Template, Admin Themes, Dashboard Template"/>
+		<meta name="keywords" content="Admin, Admin Template, Dashboard, Responsive, Admin Dashboard, Bootstrap, Bootstrap 4, Clean, Backend, Jquery, Modern, Web App, Admin Panel, Ui, Premium Admin Templates, Flat, Admin Theme, Ui Kit, Bootstrap Admin, Responsive Admin, Application, Template, Admin Themes, Dashboard Template"/> --}}
+		<title>Ezisafer</title>
 		@include('common.layout.admin-horizontal.head')
 	</head>
 
