3d84ce6dccb8c296649da9cc71a4b3e345057012
rashidali33500@gmail.com <rashidali33500@gmail.com>
2021-12-08 20:41:34 +0500

    dashtic dashoards / login register pages design

--- config/tenancy.php
+++ config/tenancy.php
@@ -22,9 +22,9 @@ return [
     'central_domains' => [
         // '127.0.0.1',
         // 'localhost',
-        // $centralDomain,
+        $centralDomain,
         // 'ezisafer.test',
-        'ezisafer.xyz',
+        // 'ezisafer.xyz',
     ],
 
     /**
@@ -199,6 +199,6 @@ return [
      */
     'seeder_parameters' => [
         '--class' => 'DatabaseSeeder', // root seeder class
-        // '--force' => true,
+        '--force' => true,
     ],
 ];
